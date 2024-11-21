ac271816f82199eb0bd66cea11a729aac515c3a2
Frederikmoeller <fmalle20@student.aau.dk>
2024-11-21 05:02:50 +0100

    Added visual detection and intuition detection.
    - Added extra animation for turning.
    - Visual field expanded to 170 degrees.
    - Can be detected when looking around.

--- VR Preset/Assets/Scripts/NPCFOV.cs
+++ VR Preset/Assets/Scripts/NPCFOV.cs
@@ -10,6 +10,7 @@ public class NPCFOV : MonoBehaviour
     [SerializeField] private LayerMask playerLayer;
     [SerializeField] private LayerMask obstacleLayer;
     [SerializeField] private Transform head;
+    [SerializeField] private CharacterController _characterController;
     [SerializeField] private float headTurnSpeed = 5f;
     [SerializeField] private float bodyRotationSpeed = 2f;
     [SerializeField] private float bodyRotationThreshold = 10f;
@@ -25,14 +26,18 @@ public class NPCFOV : MonoBehaviour
     [SerializeField] private float distanceToPlayer;
     private bool _isNewTarget;
     [SerializeField] private Vector3 _currentLookAtPosition;
-    [SerializeField] private float currentSoundThreshold;
     [SerializeField] private bool inFocusRange;
+    [SerializeField] private float minimumDetectionMoveSpeed = 1f;
+    [SerializeField] private float playerMoveSpeed;
+    [SerializeField] private bool lookingAround;
+    private float alignment;
 
     void Start()
     {
         _animator = GetComponent<Animator>();
         _player = GameObject.FindGameObjectWithTag("Player").transform;
         _playerMic = _player.GetComponentInParent<MicInput>();
+        _characterController = _player.GetComponent<CharacterController>();
 
         if (_playerMic == null)
         {
@@ -44,9 +49,13 @@ public class NPCFOV : MonoBehaviour
     
     void FixedUpdate()
     {
-        currentSoundThreshold = GetSoundThreshold();
         DetectTarget();
-        RotateBodyTowardsTarget();
+        if (!lookingAround)
+        {
+            RotateBodyAnim();
+        }
+
+        playerMoveSpeed = _characterController.velocity.magnitude;
     }
 
     void DetectTarget()
@@ -68,10 +77,15 @@ public class NPCFOV : MonoBehaviour
         foreach (var hit in hits)
         {
             // Prioritize player if they've interacted with the NPC
-            if (hit.CompareTag("Player") && _playerInteracted)
+            if (hit.CompareTag("Player"))
             {
-                bestTarget = hit.transform.GetChild(0).GetChild(0);
-                _isPlayerDetected = true;
+                //playerMoveSpeed = _player.GetComponent<CharacterController>().velocity.magnitude;
+                if (_playerInteracted)
+                {
+                    bestTarget = hit.transform.GetChild(0).GetChild(0);
+                    _isPlayerDetected = true;
+                }
+
                 break; // Stop searching if the player is found and interacted
             }
             if (hit.CompareTag("PointOfInterest"))
@@ -90,6 +104,49 @@ public class NPCFOV : MonoBehaviour
         {
             inFocusRange = true;
         }
+        
+        VisionDetection();
+
+        if (inFocusRange && _isPlayerDetected == false)
+        {
+            print(GetSpeedThreshold());
+            if (playerMoveSpeed >= GetSpeedThreshold() && lookingAround == false)
+            {
+                lookingAround = true;
+                _animator.SetBool("IsTurning", true);
+                _animator.SetBool("TurnLeft", true);
+            }
+        }
+        AnimatorStateInfo stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+        if (lookingAround)
+        {                
+            if (_playerInteracted)
+            {
+                lookingAround = false;
+                _animator.SetBool("TurnLeft", false);
+                _animator.SetBool("IsTurning", false);
+            }
+            if (stateInfo.normalizedTime >= 1.0f && !_animator.IsInTransition(0)) // Turn animation complete
+            {
+                if (_animator.GetBool("TurnLeft") && stateInfo.normalizedTime >= 1.0f)
+                {
+                    _animator.SetBool("TurnLeft", false);
+                    _animator.SetBool("TurnRight", true);
+                }
+                else if (lookingAround && stateInfo.normalizedTime >= 1.0f && playerMoveSpeed >= GetSpeedThreshold())
+                {
+                    _animator.SetBool("TurnRight", false);
+                    _animator.SetBool("TurnLeft", true);
+                }
+                else if (lookingAround && stateInfo.normalizedTime >= 1.0f && playerMoveSpeed <= GetSpeedThreshold())
+                {
+                    lookingAround = false;
+                    _animator.SetBool("IsTurning", false);
+                    _animator.SetBool("TurnRight", false);
+                }
+            }
+        }
+
 
         // Sound detection to update player detection
         if (_playerMic.soundVolume > GetSoundThreshold())
@@ -106,8 +163,22 @@ public class NPCFOV : MonoBehaviour
             _currentTarget = bestTarget;
             _isPlayerDetected = _currentTarget != null && _currentTarget.CompareTag("Player");
             _isNewTarget = true;  // Mark that we have a new target
+            Vector3 directionToTarget = (_currentTarget.position - transform.position).normalized;
+            RotateBodyTowardsTarget(directionToTarget);
         }
 
+    }
+
+    void VisionDetection()
+    {
+        Vector3 directionToTarget = (_player.position - head.position).normalized;
+        // Project vectors onto the XZ plane (ignore Y-axis)
+        float angleToTarget = Vector3.Angle(head.forward, directionToTarget);
+
+        if (angleToTarget <= fieldOfViewAngle / 2f && distanceToPlayer < detectionRadius)
+        {
+            _playerInteracted = true;
+        }
     }
 
     // Helper method to determine if the current target is valid
@@ -150,51 +221,98 @@ public class NPCFOV : MonoBehaviour
     distanceToPlayer = Vector3.Distance(head.position, _player.position);
     return Mathf.Lerp(0.5f, minimumSoundThreshold, 1 - distanceToPlayer / soundDetectionRadius);
     }
-
-    void RotateBodyTowardsTarget()
+    
+    float GetSpeedThreshold()
     {
-        if (_currentTarget == null) return;
+        // Calculate sound threshold based on distance to player
+        if (_player == null) return 1000f;
+        distanceToPlayer = Vector3.Distance(head.position, _player.position);
+        return Mathf.Lerp(3.5f, minimumDetectionMoveSpeed, 1 - distanceToPlayer / detectionRadius);
+    }
 
-        // Calculate the direction and angle to the target
-        Vector3 directionToTarget = (_currentTarget.position - transform.position).normalized;
-        float angleToTarget = Vector3.Angle(transform.forward, directionToTarget);
-
-        // Check if the angle to the target is close to or beyond the edge of the field of view
-        if (Mathf.Abs(angleToTarget - fieldOfViewAngle / 2f) >= bodyRotationThreshold || angleToTarget > fieldOfViewAngle / 2f)
-        {
-            // Get the target rotation on the Y-axis
+    void RotateBodyTowardsTarget(Vector3 directionToTarget)
+    {
+        // Get the target rotation on the Y-axis
             Quaternion targetRotation = Quaternion.LookRotation(new Vector3(directionToTarget.x, 0, directionToTarget.z));
-
+            
             // Smoothly rotate the body around the Y-axis to face the target directly
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * bodyRotationSpeed);
+    }
+
+    void RotateBodyAnim()
+    {
+        if (_currentTarget == null) return;
+        
+        Vector3 directionToTarget = (_currentTarget.position - transform.position).normalized;
+        // Project vectors onto the XZ plane (ignore Y-axis)
+        Vector3 flatForward = new Vector3(transform.forward.x, 0, transform.forward.z).normalized;
+        Vector3 flatDirectionToTarget = new Vector3(directionToTarget.x, 0, directionToTarget.z).normalized;
+        float angleToTarget = Vector3.SignedAngle(flatForward, flatDirectionToTarget, Vector3.up);
+
+        if (alignment < .75f)
+        {
+            bodyRotationSpeed = 2f;
+            RotateBodyTowardsTarget(directionToTarget);
         }
+        else if (alignment > .75)
+        {
+            bodyRotationSpeed = .5f;
+            RotateBodyTowardsTarget(directionToTarget);
+        }
+
+
+        if (angleToTarget > 0)
+        {
+            _animator.SetBool("TurnAroundLeft", true);
+            _animator.SetBool("TurnAroundRight", false);
+        }
+        else if (angleToTarget < 0)
+        {
+            _animator.SetBool("TurnAroundLeft", false);
+            _animator.SetBool("TurnAroundRight", true);
+        }
+
+
+        _animator.SetFloat("RotationLimit", alignment);
     }
 
 
     void OnAnimatorIK(int layerIndex)
     {
-
         if (_currentTarget != null)
         {
-            // Smoothly interpolate from the last target position to the new target position
-            Vector3 targetPosition = _currentTarget.position;
+            Vector3 directionToTarget = (_currentTarget.position - transform.position).normalized;
+            // Project vectors onto the XZ plane (ignore Y-axis)
+            Vector3 flatForward = new Vector3(transform.forward.x, 0, transform.forward.z).normalized;
+            Vector3 flatDirectionToTarget = new Vector3(directionToTarget.x, 0, directionToTarget.z).normalized;
+            alignment = Vector3.Dot(flatForward, flatDirectionToTarget);
+            if (alignment >= 0f)
+            {
+                // Smoothly interpolate from the last target position to the new target position
+                Vector3 targetPosition = _currentTarget.position;
 
-            /*if (_isNewTarget)
+                // Smoothly transition to the new target position
+                _currentLookAtPosition = Vector3.Lerp(_currentLookAtPosition, targetPosition, Time.deltaTime * headTurnSpeed);
+                
+                // Set look-at weight and position
+                _lookAtWeight = Mathf.Lerp(_lookAtWeight, 1f, Time.deltaTime * headTurnSpeed);
+                if (!_animator.GetBool("IsTurning"))
+                {
+                    _lookAtWeight = Mathf.Lerp(_lookAtWeight, 1f, Time.deltaTime * headTurnSpeed);
+                    _animator.SetLookAtWeight(_lookAtWeight);
+                }
+                else
+                {
+                    _lookAtWeight = 0;
+                    _animator.SetLookAtWeight(_lookAtWeight);
+                }
+                _animator.SetLookAtPosition(_currentLookAtPosition);
+            }
+            else
             {
                 _lookAtWeight = 0;
-                _isNewTarget = false;
-            }*/
-            
-            // Smoothly transition to the new target position
-            _currentLookAtPosition = Vector3.Lerp(_currentLookAtPosition, targetPosition, Time.deltaTime * headTurnSpeed);
-            
-
-            // Set look-at weight and position
-            _lookAtWeight = Mathf.Lerp(_lookAtWeight, 1f, Time.deltaTime * headTurnSpeed);
-            _animator.SetLookAtWeight(_lookAtWeight);
-            _animator.SetLookAtPosition(_currentLookAtPosition);
-
-            //Debug.Log($"Smoothing towards target position at {targetPosition}, Weight: {_lookAtWeight}");
+                _animator.SetLookAtWeight(_lookAtWeight);
+            }
         }
         else
         {
@@ -204,7 +322,7 @@ public class NPCFOV : MonoBehaviour
         }
     }
 
-    /*void OnDrawGizmos()
+    void OnDrawGizmos()
     {
         // Visualize detection radius and field of view
         Gizmos.color = Color.red;
@@ -216,5 +334,5 @@ public class NPCFOV : MonoBehaviour
         Gizmos.color = Color.yellow;
         Gizmos.DrawLine(head.position, head.position + leftBoundary);
         Gizmos.DrawLine(head.position, head.position + rightBoundary);
-    }*/
+    }
 }
